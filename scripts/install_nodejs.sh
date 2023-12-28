c4678987830f339be44ccdd8ff51818dc865da25
mingster <mingster@users.noreply.github.com>
2023-12-28 14:19:15 +0800

    Update install_nodejs.sh

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 # Create deb repository
-NODE_MAJOR=21
+NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # Run Update and Install
